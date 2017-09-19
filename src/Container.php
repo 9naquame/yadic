daac9ed96a76b801c73a6a8baae0a3ba6d580d32
Nana Kwame <9naquame@gmail.com>
2017-09-19 20:11:49 +0000

    container underway

---
+++ src/Container.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace orignx\diject;
+
+use Psr\Container\ContainerInterface;
+
+/**
+ * Container class through which dependencies are defined and resolved.
+ *
+ * @author ekow
+ */
+class Container implements ContainerInterface
+{
+    public function get($id)
+    {
+        
+    }
+    
+    public function has($id)
+    {
+        
+    }
+}
